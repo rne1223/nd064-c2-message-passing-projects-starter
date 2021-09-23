6007ccf89fbb9ce854675c60752d8aa1731def88
rne1223 <rne1223@gmail.com>
2021-09-23 00:51:59 +0000

    changed names to reflect yaml intention and added a new svc to port 30009 to get debugging features

--- modules/api/app/__init__.py
+++ modules/api/app/__init__.py
@@ -5,19 +5,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-print("------HELLO World------")
 def create_app(env=None):
-    from app.config import config_by_name
-    from app.routes import register_routes
+    # from app.config import config_by_name
+    # from app.routes import register_routes
 
     app = Flask(__name__)
-    app.config.from_object(config_by_name[env or "test"])
-    api = Api(app, title="UdaConnect API", version="0.1.0")
+    # app.config.from_object(config_by_name[env or "test"])
+    # api = Api(app, title="UdaConnect API", version="0.1.0")
 
-    CORS(app)  # Set CORS for development
+    # CORS(app)  # Set CORS for development
 
-    register_routes(api, app)
-    db.init_app(app)
+    # register_routes(api, app)
+    # db.init_app(app)
 
     @app.route("/health")
     def health():
