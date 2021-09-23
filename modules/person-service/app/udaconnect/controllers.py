600459e247e0720eee5e07099edfccd23109638c
rne1223 <rne1223@gmail.com>
2021-09-23 19:15:06 +0000

    Carefully modifying person service

--- modules/person-service/app/udaconnect/controllers.py
+++ modules/person-service/app/udaconnect/controllers.py
@@ -14,7 +14,7 @@ from typing import Optional, List
 
 DATE_FORMAT = "%Y-%m-%d"
 
-api = Namespace("UdaConnect", description="Connections via geolocation.")  # noqa
+api = Namespace("Persons API", description="All person related stuff")  # noqa
 
 
 # TODO: This needs better exception handling
--- scripts/argocd_install.sh
+++ scripts/argocd_install.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 ARGOCD_DIR="$HOME/udaconnect/argocd"
-NAMESPACE='argocd'
+ARGOCD_NAMESPACE='argocd'
+APP_NAMESPACE='udaconnect'
+
+# create namespaces
+kubectl create namespace $ARGOCD_NAMESPACE
+kubectl create namespace $APP_NAMESPACE
+
 
 # deploy ArgoCD
-kubectl create namespace $NAMESPACE
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # NodePort to access argocd ui
-kubectl apply -n $NAMESPACE -f "$ARGOCD_DIR/argocd-server-nodeport.yml"
+kubectl apply -n $ARGOCD_NAMESPACE -f "$ARGOCD_DIR/argocd-server-nodeport.yml"
 
 kubectl -n argocd get pod -w
 
 # Installing the application
-kubectl apply -n $NAMESPACE -f "$ARGOCD_DIR/udaconnect-application.yml" 
+kubectl apply -n $ARGOCD_NAMESPACE -f "$ARGOCD_DIR/udaconnect-application.yml" 
