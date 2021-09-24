dc015695a0f86837d7d368836f75a96b9c428c1c
rne1223 <rne1223@gmail.com>
2021-09-24 09:05:26 +0000

    Changed the person-service to use the udaconnect-base image

--- modules/api/app/udaconnect/services.py
+++ modules/api/app/udaconnect/services.py
@@ -136,7 +136,7 @@ class PersonService:
     @staticmethod
     def retrieve_all() -> List[Person]:
         # Use person-service to retrieve all persons from DB
-        # persons = requests.get("http://udaconnect-person-service:5001/api/persons")
+        persons = requests.get("http://udaconnect-person-service:5001/api/persons")
         # logging.info("Persons return:", persons)
-        return db.session.query(Person).all()
-        # return persons
+        # return db.session.query(Person).all()
+        return persons
