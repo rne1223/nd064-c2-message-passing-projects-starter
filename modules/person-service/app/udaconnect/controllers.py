23f8814910fc8bd11f7cb7f04659829f7e3abc27
rne1223 <rne1223@gmail.com>
2021-09-27 17:30:17 +0000

    Being able to add person through person-service-api

--- modules/person-service/app/udaconnect/controllers.py
+++ modules/person-service/app/udaconnect/controllers.py
@@ -1,13 +1,9 @@
 from datetime import datetime
 import logging
 
-from app.udaconnect.models import Connection, Location, Person
-from app.udaconnect.schemas import (
-    ConnectionSchema,
-    LocationSchema,
-    PersonSchema,
-)
-from app.udaconnect.services import ConnectionService, LocationService, PersonService
+from app.udaconnect.models import Person
+from app.udaconnect.schemas import PersonSchema
+from app.udaconnect.services import PersonService
 from flask import request
 from flask_accepts import accepts, responds
 from flask_restx import Namespace, Resource
