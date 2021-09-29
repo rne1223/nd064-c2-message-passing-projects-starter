7388b55b1f1198186015e6385459681d06cb7ec2
rne1223 <rne1223@gmail.com>
2021-09-29 00:17:47 +0000

    Added some db scripts to cleanup the DB

--- scripts/run_db_command.sh
+++ scripts/run_db_command.sh
@@ -4,10 +4,13 @@
 export CT_DB_USERNAME=ct_admin
 export CT_DB_NAME=geoconnections
 
-echo $1
-echo $2
-cat ~/udaconnect/db/2020-08-15_init-db.sql | kubectl -n $1 exec -i $2 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
+# echo $1
+# echo $2
 
-cat ~/udaconnect/db/udaconnect_public_person.sql | kubectl -n $1 exec -i $2 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
+# cat ~/udaconnect/db/dropDB.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
 
-cat ~/udaconnect/db/udaconnect_public_location.sql | kubectl -n $1 exec -i $2 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
+cat ~/udaconnect/db/2020-08-15_init-db.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
+
+cat ~/udaconnect/db/udaconnect_public_person.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
+
+cat ~/udaconnect/db/udaconnect_public_location.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
